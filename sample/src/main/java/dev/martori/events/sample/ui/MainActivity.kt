f68c8f55e052e2a0d6ba6331e4f2a5faed87f13e
Eric Martori <eric.martori.l@gmail.com>
2019-08-29 19:22:31 +0200

    add navigation

--- sample/src/main/java/dev/martori/events/sample/ui/MainActivity.kt
+++ sample/src/main/java/dev/martori/events/sample/ui/MainActivity.kt
@@ -1,6 +1,20 @@
 package dev.martori.events.sample.ui
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
 import dev.martori.events.sample.R
+import dev.martori.events.sample.binding.services.Navigator
+import kotlinx.android.synthetic.main.activity_main.*
+import org.koin.core.context.loadKoinModules
+import org.koin.dsl.module
 
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private val mainModule = module {
+        single<Navigator> { NavHostNavigator(navHostFragment.findNavController()) }
+    }
+
+    init {
+        loadKoinModules(mainModule)
+    }
+}
--- sample/src/main/java/dev/martori/events/sample/ui/MainListFragment.kt
+++ sample/src/main/java/dev/martori/events/sample/ui/MainListFragment.kt
@@ -1,7 +1,23 @@
 package dev.martori.events.sample.ui
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import dev.martori.events.android.outEvent
+import dev.martori.events.core.OutEvent
 import dev.martori.events.sample.R
+import dev.martori.events.sample.binding.binds.bindListNavigation
+import dev.martori.events.sample.binding.views.MainListView
+import kotlinx.android.synthetic.main.fragment_main_list.*
+import org.koin.android.ext.android.get
 
-class MainListFragment : Fragment(R.layout.fragment_main_list) {
+class MainListFragment : Fragment(R.layout.fragment_main_list), MainListView {
+
+    override val openDetails: OutEvent<Int> = outEvent()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bindListNavigation(this, get())
+        openDetailsButton.setOnClickListener { openDetails(0) }
+    }
 }
