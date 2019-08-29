5a46d53fef03eac5c36f2025cc0e42d14eaf15d4
Eric Martori <eric.martori.l@gmail.com>
2019-08-29 19:56:37 +0200

    complete navigation

--- sample/src/main/java/dev/martori/events/sample/ui/DetailsFragment.kt
+++ sample/src/main/java/dev/martori/events/sample/ui/DetailsFragment.kt
@@ -1,7 +1,29 @@
 package dev.martori.events.sample.ui
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import dev.martori.events.android.outEvent
+import dev.martori.events.core.OutEventU
+import dev.martori.events.core.invoke
 import dev.martori.events.sample.R
+import dev.martori.events.sample.binding.binds.bindDetailsNavigation
+import dev.martori.events.sample.binding.views.DetailView
+import kotlinx.android.synthetic.main.fragment_details.*
+import org.koin.android.ext.android.get
 
-class DetailsFragment : Fragment(R.layout.fragment_details) {
+class DetailsFragment : Fragment(R.layout.fragment_details), DetailView {
+    init {
+        lifecycleScope.launchWhenCreated {
+            bindDetailsNavigation(this@DetailsFragment, get())
+        }
+    }
+
+    override val goBack: OutEventU = outEvent()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        goBackButton.setOnClickListener { goBack() }
+    }
 }
