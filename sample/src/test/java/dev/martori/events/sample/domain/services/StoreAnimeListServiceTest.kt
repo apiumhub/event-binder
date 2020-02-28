64fff5348905c6dbaf6e113baf8d4660cba5fd87
Eric <eric.martori.l@gmail.com>
2020-02-28 16:25:28 +0100

    add more tests

--- sample/src/test/java/dev/martori/events/sample/domain/services/StoreAnimeListServiceTest.kt
+++ sample/src/test/java/dev/martori/events/sample/domain/services/StoreAnimeListServiceTest.kt
@@ -3,7 +3,10 @@ package dev.martori.events.sample.domain.services
 import dev.martori.events.sample.binding.models.AnimeRequest
 import dev.martori.events.sample.domain.entities.Anime
 import dev.martori.events.sample.domain.repositories.Repository
+import dev.martori.events.test.Parameter
+import dev.martori.events.test.shouldDispatch
 import dev.martori.events.test.withParameter
+import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.test.runBlockingTest
@@ -19,5 +22,33 @@ class StoreAnimeListServiceTest {
     fun `loadanime makes a request to the repository with the provided request`() = runBlockingTest {
         (sut.loadAnime withParameter request).dispatch()
         coVerify { repository.get(request) }
+    }
+
+    @Test
+    fun `load anime dispatches fetch event`() {
+        sut.loadAnime withParameter request shouldDispatch {
+            sut.startFetching withParameter Unit
+        }
+    }
+
+    @Test
+    fun `load anime dispatches fetch and error events if request fails`() {
+        coEvery { repository.get(any()) } throws Error()
+
+        sut.loadAnime withParameter request shouldDispatch {
+            sut.startFetching withParameter Unit
+            sut.errorReceived withAny Parameter
+        }
+    }
+
+    @Test
+    fun `load anime dispatches fetch and success events if request returns`() {
+        val result = listOf<Anime>()
+        coEvery { repository.get(any()) } returns result
+
+        sut.loadAnime withParameter request shouldDispatch {
+            sut.startFetching withParameter Unit
+            sut.animeListReceived withParameter result
+        }
     }
 }
